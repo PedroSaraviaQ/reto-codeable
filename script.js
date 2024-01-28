c60680e8770a0d3a408017151ac04ec97affe25a
Pedro <pedro.saravia.q@uni.pe>
2024-01-28 01:12:47 -0500

    recordar notas al recargar pagina

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
 //* Crear funciones para cada tarea, tambien refactoriza
 
 const contenedorNotas = document.getElementById("contenedor");
-
 const inputNuevaNota = document.getElementsByTagName("textarea")[0];
-
 const botonCrearNota = document.getElementById("crear");
 
-function crearNota(e) {
-    e.preventDefault();
+document.addEventListener("DOMContentLoaded", () => {
+    let notasGuardadas = JSON.parse(localStorage.getItem("notas")) || [];
+    notasGuardadas.forEach(element => crearNota(element));
+})
 
+function notaOnClick(e) {
+    e.preventDefault();
+    crearNota(inputNuevaNota.value);
+    inputNuevaNota.value = "";
+}
+
+function crearNota(texto) {
     const nuevaNota = document.createElement("div");
     nuevaNota.className = "nota";
 
     const paragrafoNota = document.createElement("p");
-    paragrafoNota.textContent = inputNuevaNota.value;
+    paragrafoNota.textContent = texto;
 
     const botonBorrar = document.createElement("button");
     botonBorrar.className = "borrar";
@@ -22,11 +29,18 @@ function crearNota(e) {
 
     nuevaNota.appendChild(paragrafoNota);
     nuevaNota.appendChild(botonBorrar);
-
     contenedorNotas.appendChild(nuevaNota);
+    guardarNotas();
 }
 
 function eliminarNota(e) {
     const nota = e.target.parentElement;
     contenedorNotas.removeChild(nota);
+    guardarNotas();
+}
+
+function guardarNotas() {
+    let notasGuardadas = [...document.querySelectorAll(".nota p")];
+    notasGuardadas = notasGuardadas.map(n => n.textContent);
+    localStorage.setItem("notas", JSON.stringify(notasGuardadas));
 }
