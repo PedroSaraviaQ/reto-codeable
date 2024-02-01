c8a72b827d086739b040ad2465534959faa364d7
Pedro <pedro.saravia.q@uni.pe>
2024-01-31 20:19:39 -0500

    add live code

--- script.js
+++ script.js
@@ -5,8 +5,11 @@ const noteInput = document.querySelector("textarea");
 //& Function to create a new note when the create button is clicked
 function createNote(e) {
     e.preventDefault();
-    const text = noteInput.value;
-    if (text === "") return;
+    const text = noteInput.value.trim();
+    if (text === "") {
+        alert("Por favor, colocar un texto");
+        return;
+    }
 
     createNoteElement(text);
     noteInput.value = "";
@@ -24,13 +27,19 @@ function createNoteElement(text) {
     noteTextParagraph.textContent = text;
 
     const deleteNoteButton = document.createElement("button");
-    deleteNoteButton.classList.add("btn", "delete");
+    deleteNoteButton.classList.add("btn", "note_btn");
     deleteNoteButton.textContent = "Borrar";
     deleteNoteButton.addEventListener("click", deleteNote);
+
+    const editNoteButton = document.createElement("button");
+    editNoteButton.classList.add("btn", "note_btn");
+    editNoteButton.textContent = "Editar"
+    editNoteButton.addEventListener("click", editNote);
 
     //* Append elements to the note container
     newNote.appendChild(noteTextParagraph);
     newNote.appendChild(deleteNoteButton);
+    newNote.appendChild(editNoteButton);
     noteContainer.appendChild(newNote);
 }
 
@@ -44,6 +53,15 @@ function saveNotes() {
 function deleteNote() {
     const note = this.parentElement;
     noteContainer.removeChild(note);
+    saveNotes();
+}
+
+//& Function to edit a note
+function editNote() {
+    const note = this.parentElement;
+    const paragraph = note.querySelector("p");
+    const newText = prompt("Por favor, ingresar nuevo texto");
+    paragraph.textContent = newText;
     saveNotes();
 }
 
